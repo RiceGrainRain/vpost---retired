95f11ac96925346cbfe7781392b2db9faacd0508
RiceGrainRain <mnmanasnavale@gmail.com>
2023-07-29 10:04:39 -0500

    "login page"

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 189, 189, 189)),
+          ),
+          fillColor: Color.fromRGBO(238, 238, 238, 1),
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:projects/components/my_text_field.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  //controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const [
+              //logo
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(height: 50),
+
+              //username
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+              //password
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: false,
+              ),
+              //forgot password
+              //sign in
+              //continue with
+              //google + apple
+              //not a member?
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
